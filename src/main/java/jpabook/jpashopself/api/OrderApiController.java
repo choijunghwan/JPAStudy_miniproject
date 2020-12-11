c80ebd3d3a4843f1236a58c30c51ac6100f796dd
Choi Jung Hwan <vkdlxj3562@naver.com>
2020-12-11 15:59:26 +0900

    XToMany 가 포함되어 있을때 JPA에서 DTO로 직접조회 최적화, 플랫데이터 최적화

--- src/main/java/jpabook/jpashopself/api/OrderApiController.java
+++ src/main/java/jpabook/jpashopself/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashopself.api;
 
 import jpabook.jpashopself.domain.*;
 import jpabook.jpashopself.repository.OrderRepository;
+import jpabook.jpashopself.repository.order.query.OrderFlatDto;
+import jpabook.jpashopself.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashopself.repository.order.query.OrderQueryDto;
 import jpabook.jpashopself.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,7 +44,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAll(new OrderSearch());
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return collect;
     }
@@ -55,7 +59,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
     }
@@ -69,7 +73,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
     }
@@ -78,6 +82,28 @@ public class OrderApiController {
     public List<OrderQueryDto> ordersV4() {
         return  orderQueryRepository.findOrderQueryDtos();
     }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> orderV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }
+
 
     // no properties는 자바에서 보통 Getter, Setter가 없다는 오류이다
     @Getter
@@ -98,7 +124,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             orderItems = order.getOrderItems().stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
