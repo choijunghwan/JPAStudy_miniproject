7d57514d02d47cfcb6bccff505c0b1e394fdaad4
Choi Jung Hwan <vkdlxj3562@naver.com>
2020-12-16 22:38:25 +0900

    회원등록 API review

---
+++ src/main/java/jpabook/jpashopself/api/MemberApiControllerSelf.java
@@ -0,0 +1,68 @@
+package jpabook.jpashopself.api;
+
+import jpabook.jpashopself.domain.Address;
+import jpabook.jpashopself.domain.Member;
+import jpabook.jpashopself.repository.MemberRepository;
+import jpabook.jpashopself.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+// controller에서 Data를 반환할때 ResponseBody를 이용해 Json 형식으로 데이터를 반환해야한다.
+// RestController는 Controller + ResponseBody로 Json형태로 객체 데이터 반환까지 가능하다.
+@RestController
+@RequiredArgsConstructor
+public class MemberApiControllerSelf {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원 등록V1 : 요청 값으로 Member 엔티티를 직접 받는다
+     * 단점: 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * 엔티티에 API 검증을 위한 로직이 들어간다.
+     * 엔티티가 변경되면 API 스펙이 변한다.
+     */
+    @PostMapping("/api/save/v1/members")
+    public CreateMemberResponseSelf saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponseSelf(id);
+    }
+
+    @Data
+    static class CreateMemberResponseSelf {
+        private Long id;
+
+        public CreateMemberResponseSelf(Long id) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * 회원등록V2 : 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     * @RequestBody는 HTTP 요청 몸체를 자바 객체로 전달받을 수 있다.
+     */
+    @PostMapping("/api/save/v2/members")
+    public CreateMemberResponseSelf saveMemberV2(@RequestBody @Valid CreateMemberRequestSelf request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Address address = new Address(request.getCity(), request.getStreet(), request.getZipcode());
+        member.setAddress(address);
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponseSelf(id);
+    }
+
+    @Data
+    static class CreateMemberRequestSelf {
+        private String name;
+        private String city;
+        private String street;
+        private String zipcode;
+    }
+
+}
